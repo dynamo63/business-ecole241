3e595f438d3a012dbc7e3c22e00d55b22fe77307
nickqadosh <nickqadosh@gmail.com>
2020-10-25 17:40:16 +0100

    crud_requêtes

---
+++ application/models/Commercial_model.php
@@ -0,0 +1,69 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Commercial_model extends CI_Model
+{
+
+    public $nom_prenom;
+    public $num_tel;
+    public $num_what;
+    public $email;
+    public $sexe;
+    public $date_n;
+    public $nom_user;
+    public $mot_passe;
+
+    public function __construct()
+    {
+        $this->load->database();
+    }
+
+    public function tous_les_commerciaux()
+    {
+        $query = $this->db->get('commercial', );
+        return $query->result();
+    }
+
+    public function inserer_commercial($params)
+    {
+        $this->nom_prenom = $params['nom_prenom']; // please read the below note
+        $this->num_tel    = $params['num_tel'];
+        $this->num_what   = $params['num_what'];
+        $this->email      = $params['email'];
+        $this->sexe       = $params['sexe'];
+        $this->date_n     = $params['date_n'];
+        $this->nom_user   = $params['nom_user'];
+        $this->mot_passe  = $params['mot_passe'];
+
+        $this->db->insert('commercial', $this);
+    }
+
+    public function modifier_commercial($params)
+    {
+        $this->nom_prenom = $params['nom_prenom']; // please read the below note
+        $this->num_tel    = $params['num_tel'];
+        $this->num_what   = $params['num_what'];
+        $this->email      = $params['email'];
+        $this->sexe       = $params['sexe'];
+        $this->date_n     = $params['date_n'];
+        $this->nom_user   = $params['nom_user'];
+        $this->mot_passe  = $params['mot_passe'];
+
+        $this->db->update('entries', $this, array('id' => $params['id']));
+    }
+
+    public function supprimer_commercial($params)
+    {
+        $this->nom_prenom = $params['nom_prenom']; // please read the below note
+        $this->num_tel    = $params['num_tel'];
+        $this->num_what   = $params['num_what'];
+        $this->email      = $params['email'];
+        $this->sexe       = $params['sexe'];
+        $this->date_n     = $params['date_n'];
+        $this->nom_user   = $params['nom_user'];
+        $this->mot_passe  = $params['mot_passe'];
+
+        $this->db ->delete('commercial',array('id' =>$params['id']));
+    }
+
+}
